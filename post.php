84d5543b7075c7addccd2ba83b3d19b2efd5575f
LucasC78 <lucascoutantpro@gmail.com>
2023-07-28 16:49:30 +0200

    first push

--- post.php
+++ post.php
@@ -2,9 +2,9 @@
 include('template.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Vérifier si toutes les données requises sont présentes
+   
     if (!empty($_POST['name']) && !empty($_POST['price'])) {
-        // Récupérer les données du corps de la requête POST
+       
         $name = $_POST['name'];
         $price = $_POST['price'];
 
@@ -14,14 +14,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $requete->bindParam(':price', $price);
 
         if ($requete->execute()) {
-            // Le produit a été ajouté avec succès
+           
             echo "Le produit a bien été ajouté.";
         } else {
-            // Une erreur s'est produite lors de l'ajout du produit
+            
             echo "Une erreur s'est produite lors de l'ajout du produit.";
         }
     } else {
-        // Des informations requises sont manquantes
+       
         echo "Il manque des informations pour ajouter un produit.";
     }
 }
